e130e54d3a59924b097adee8dc06cfbfa6bf559d
Dhipa Poltak <dhipa2poltak@gmail.com>
2023-05-13 04:06:55 +0700

    adding "keep" annotation

--- framework/src/main/java/com/dpfht/android/demo62teknologi/framework/data/core/api/rest/ErrorResponse.kt
+++ framework/src/main/java/com/dpfht/android/demo62teknologi/framework/data/core/api/rest/ErrorResponse.kt
@@ -1,9 +1,13 @@
 package com.dpfht.android.demo62teknologi.framework.data.core.api.rest
 
+import androidx.annotation.Keep
+
+@Keep
 data class ErrorResponse(
   val error: Error?
 )
 
+@Keep
 data class Error(
   val code: String = "",
   val description: String = ""
